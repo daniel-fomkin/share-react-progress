2b8f6d06309816a5ab93c5aa0f88e26bb445fca2
daniel-fomkin <danielfomkin87@gmail.com>
2026-04-03 12:20:52 +0400

    Update Directions.jsx

--- src/components/Directions.jsx
+++ src/components/Directions.jsx
@@ -4,6 +4,8 @@ import upArrowImage from '../assets/up.svg';
 
 import PageAnimation from './PageAnimation';
 
+import translate from '../utlils/translate';
+
 //Componentas puslapiui su kryptimi, su kalbos parametru
 function Directions({lang}){
 
@@ -11,39 +13,19 @@ function Directions({lang}){
 
     const goToConfrimation = () => navigate("/confrimation");
 
-    //Componento lietuviskas ir angliskas tekstas
-    const text = [
-    {
-      LT:"Jūsų kelionė prasideda čia",
-      EN:"Your journey begins here."
-    },
-    {
-      LT:"Baigti",
-      EN:"Finish"
-    },
-    {
-      LT: "Eikite į antrą aukštą",
-      EN: "Go to the second floor."
-    },
-    {
-      LT: "Jūsų laukia 205 kabinete",
-      EN: "You are expected in room 205."
-    }
-  ];
-
     return(
         <PageAnimation>
           <div className="directions">
               <img src={checkImage} alt='check' />
-              <h1>{text[0][lang]}</h1>
+              <h1>{translate("directions", "header", lang)}</h1>
               <div className="directions_content">
                   <img src={upArrowImage} alt="up arrow" />
                   <div className="directions_text">
-                      <p>{text[2][lang]}</p>
-                      <p>{text[3][lang]}</p>
+                      <p>{translate("directions", "directionText", lang)}</p>
+                      <p>{translate("directions", "cabinet", lang)}</p>
                   </div>
               </div>
-              <button type="button" className='directions-btn' onClick={goToConfrimation}>{text[1][lang]}</button>
+              <button type="button" className='directions-btn' onClick={goToConfrimation}>{translate("directions", "finishButton", lang)}</button>
           </div>
         </PageAnimation>
     );
