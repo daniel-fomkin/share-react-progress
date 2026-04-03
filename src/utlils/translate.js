6125c98d183ada457c7aaf41e6c4339a9f8db45e
daniel-fomkin <danielfomkin87@gmail.com>
2026-04-03 12:05:48 +0400

    Update translate.js

--- src/utlils/translate.js
+++ src/utlils/translate.js
@@ -16,6 +16,65 @@ const translate = (page, word, lang) => {
               LT: "Pradėti registraciją",
               EN: "Start registration",
             }
+        },
+        registration: {
+          header: {
+            LT:"Lankytojo registracija",
+            EN:"Visitor Registration"
+          },
+          nameText: {
+            LT: "Vardas",
+            EN: "Name",
+          
+          },
+          lastNameText: {
+            LT: "Pavardė",
+            EN: "Last name",
+          },
+          goalText: {
+            LT: "Vizito tikslas",
+            EN: "The purpose of visit",
+          },
+          adreseeText: {
+            LT: "Vizitas pas",
+            EN: "Who did you come to visit",
+          },
+          continueButton: {
+            LT: "Testi",
+            EN: "Continue",
+          },
+          backButton: {
+            LT: "Atgal",
+            EN: "Back",
+          },
+          schoolGoal: {
+            LT:"Dėl mokslų",
+            EN: "For studies"
+          },
+          documentGoal: {
+            LT:"Dėl dokumentų",
+            EN:"For documents"
+          },
+          consultationGoal: {
+            LT:"Konsultacijai",
+            EN:"For consultation"
+          },
+          plannedGoal: {
+            LT:"Suderintas vizitas",
+            EN:"Planned visit"
+          },
+          otherGoal: {
+            LT:"Kita",
+            EN:"Other"
+          },
+          reason: {
+            LT:"Įrašyk priežastį",
+            EN:"Enter the reason"
+          },
+          email: {
+            LT: "Jūsų EL. paštas",
+            EN: "Your email"
+          } 
         }
     }
 
