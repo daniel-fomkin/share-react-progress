dac85d6e33e5d6ebca5ac56f554b0266c8352cc9
vikacheburika <vikacheburika@gmail.com>
2026-05-10 21:56:34 +0300

    Translated the directions in 'how to get there'

--- src/utlils/data.js
+++ src/utlils/data.js
@@ -16,56 +16,65 @@ const teacherData = [
       email: "alina.vaitekuniene@techin.lt",
       cabinet: 203,
       floor: 2,
-      direction: "Eikite koridoriumi iki laiptų kairėje → pakilkite į antrą aukštą → eikite tiesiai koridoriumi → 203 kabinetas"
+      direction: "Eikite koridoriumi iki laiptų kairėje → pakilkite į antrą aukštą → eikite tiesiai koridoriumi → 203 kabinetas",
+      directionENG: "Go down the corridor to the stairs on the left → go up to the second floor → walk straight down the corridor → room 203."
     },
     {
       name: "Mokymų vadybininkė Agata Potkina",
       email: "agata.potkina@techin.lt",
       cabinet: 100,
       floor: 1,
-      direction: "Įėjus į pastatą → pirmame aukšte, 100 kabinetas"
+      direction: "Įėjus į pastatą → pirmame aukšte, 100 kabinetas",
+      directionENG: "After entering the building → on the first floor, room 100."
     },
     {
       name: "Profesijos mokytoja (Apskaitininko mokymo programa) Laima Žvybienė",
       email: "laima.zvybiene@techin.lt",
       cabinet: 101,
       floor: -1,
-      direction: "Eikite koridoriumi iki laiptų dešinėje → nusileiskite laiptais žemyn → pasukite į kairę → 101 kabinetas"
+      direction: "Eikite koridoriumi iki laiptų dešinėje → nusileiskite laiptais žemyn → pasukite į kairę → 101 kabinetas",
+      directionENG: "Go down the corridor to the stairs on the right → go downstairs → turn left → room 101."
+      
     },
     {
       name: "Profesijos mokytoja (Finansinių paslaugų teikėjo mokymo programa) Jolanta Paulauskienė",
       email: "jolanta.paulauskiene@techin.lt",
       cabinet: 102,
       floor: -1,
-      direction: "Eikite koridoriumi iki laiptų dešinėje → nusileiskite laiptais žemyn → pasukite į kairę → 102 kabinetas"
+      direction: "Eikite koridoriumi iki laiptų dešinėje → nusileiskite laiptais žemyn → pasukite į kairę → 102 kabinetas",
+      directionENG: "Go down the corridor to the stairs on the right → go downstairs → turn left → room 102."
     },
     {
       name: "Profesijos mokytojas (Finansinių paslaugų teikėjo mokymo programa) Darius Voišnis",
       email: "darius.voisnis@techin.lt",
       cabinet: 103,
       floor: -1,
-      direction: "Eikite koridoriumi iki laiptų dešinėje → nusileiskite laiptais žemyn → pasukite į kairę → 103 kabinetas"
+      direction: "Eikite koridoriumi iki laiptų dešinėje → nusileiskite laiptais žemyn → pasukite į kairę → 103 kabinetas",
+      directionENG: "Go down the corridor to the stairs on the right → go downstairs → turn left → room 103."
     },
     {
       name: "Profesijos mokytojas (Oro uosto keleivių aptarnavimo specialisto mokymo programa) Miroslav Radulevič",
       email: "miroslav.radulevic@techin.lt",
       cabinet: 106,
       floor: -1,
-      direction: "Eikite koridoriumi iki laiptų dešinėje → nusileiskite laiptais žemyn → pasukite į dešinę → 106 kabinetas"
+      direction: "Eikite koridoriumi iki laiptų dešinėje → nusileiskite laiptais žemyn → pasukite į dešinę → 106 kabinetas",
+      directionENG: "Go down the corridor to the stairs on the right → go downstairs → turn right → room 106."
     },
     {
       name: "Profesijos mokytoja (Biuro administratoriaus mokymo programa) Indrė Jasiulionienė",
       email: "indre.jasiulioniene@techin.lt",
       cabinet: 201,
       floor: 2,
-      direction: "Eikite koridoriumi iki laiptų kairėje → pakilkite į antrą aukštą → eikite tiesiai → pasukite į kairę → 201 kabinetas"
+      direction: "Eikite koridoriumi iki laiptų kairėje → pakilkite į antrą aukštą → eikite tiesiai → pasukite į kairę → 201 kabinetas",
+      directionENG: "Go down the corridor to the stairs on the left → go up to the second floor → go straight → turn left → room 201."
     },
     {
       name: "Profesijos mokytoja (Biuro administravimo mokymo programa) Virginija Puodžiukienė",
       email: "virginija.puodziukiene@techin.lt",
       cabinet: 202,
       floor: 2,
-      direction: "Eikite koridoriumi iki laiptų kairėje → pakilkite į antrą aukštą → eikite tiesiai → pasukite į kairę → 202 kabinetas"
+      direction: "Eikite koridoriumi iki laiptų kairėje → pakilkite į antrą aukštą → eikite tiesiai → pasukite į kairę → 202 kabinetas",
+      directionENG: "Go down the corridor to the stairs on the left → go up to the second floor → go straight → turn left → room 202."
     }
   ];
 
